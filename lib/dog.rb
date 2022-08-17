763707203ceea2126e9c7d3c90d775ae6c8ff64b
wandiamugo <wwandia173@gmail.com>
2022-08-17 21:10:37 +0300

    commit tree b613ff9b479e4e0c5c634d76aa4ad153d52d1a60

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+    end
+  
+  end
+
+class Dog
+    attr_reader :breed
+  
+    def initialize(breed)
+      @breed = "Mutt"
+    end
+  
+  end
+
+ 
